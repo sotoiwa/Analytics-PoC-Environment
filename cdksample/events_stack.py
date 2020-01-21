698f070c894260e27812de1a445f79aa1e2d9d39
sotoiwa <sotoiwa@gmail.com>
2020-01-22 02:24:15 +0900

    update

--- cdksample/events_stack.py
+++ cdksample/events_stack.py
@@ -30,59 +30,69 @@ class EventsStack(core.Stack):
             )
         )
 
-        ec2_rule = events.Rule(
-            self, 'Ec2Rule',
-            event_pattern=events.EventPattern(
-                source=['aws.ec2'],
-                detail_type=['AWS API Call via CloudTrail'],
-                detail={
-                    "eventSource": [
-                        "ec2.amazonaws.com"
-                    ],
-                    "eventName": [
-                        "AttachInternetGateway",
-                        "AssociateRouteTable",
-                        "CreateRoute",
-                        "DeleteCustomerGateway",
-                        "DeleteInternetGateway",
-                        "DeleteRoute",
-                        "DeleteRouteTable",
-                        "DeleteDhcpOptions",
-                        "DisassociateRouteTable"
-                        "RunInstances",
-                        "CreateInstances",
-                        "LaunchInstances",
-                        "TerminateInstances",
-                        "AuthorizeSecurityGroupIngress",
-                        "AuthorizeSecurityGroupEgress",
-                        "RevokeSecurityGroupIngress",
-                        "RevokeSecurityGroupEgress",
-                        "CreateSecurityGroup",
-                        "DeleteSecurityGroup",
-                    ]
-                }
-            )
-        )
-        ec2_rule.add_target(targets.SnsTopic(alert_topic))
+        ec2_event_names = [
+            "AttachInternetGateway",
+            "AssociateRouteTable",
+            "CreateRoute",
+            "DeleteCustomerGateway",
+            "DeleteInternetGateway",
+            "DeleteRoute",
+            "DeleteRouteTable",
+            "DeleteDhcpOptions",
+            "DisassociateRouteTable"
+            "RunInstances",
+            "CreateInstances",
+            "LaunchInstances",
+            "TerminateInstances",
+            "AuthorizeSecurityGroupIngress",
+            "AuthorizeSecurityGroupEgress",
+            "RevokeSecurityGroupIngress",
+            "RevokeSecurityGroupEgress",
+            "CreateSecurityGroup",
+            "DeleteSecurityGroup"
+        ]
 
-        cloudtrail_rule = events.Rule(
-            self, 'CloudTrailRule',
-            event_pattern=events.EventPattern(
-                source=['aws.cloudtrail'],
-                detail_type=['AWS API Call via CloudTrail'],
-                detail={
-                    "eventSource": [
-                        "cloudtrail.amazonaws.com"
-                    ],
-                    "eventName": [
-                        "StopLogging",
-                        "DeleteTrail",
-                        "UpdateTrail"
-                    ]
-                }
+        for event_name in ec2_event_names:
+            ec2_rule = events.Rule(
+                self, 'Ec2{}Rule'.format(event_name),
+                event_pattern=events.EventPattern(
+                    source=['aws.ec2'],
+                    detail_type=['AWS API Call via CloudTrail'],
+                    detail={
+                        "eventSource": [
+                            "ec2.amazonaws.com"
+                        ],
+                        "eventName": [
+                            event_name
+                        ]
+                    }
+                )
             )
-        )
-        cloudtrail_rule.add_target(targets.SnsTopic(alert_topic))
+            ec2_rule.add_target(targets.SnsTopic(alert_topic))
+
+        cloudtrail_event_names = [
+            "StopLogging",
+            "DeleteTrail",
+            "UpdateTrail"
+        ]
+
+        for event_name in cloudtrail_event_names:
+            cloudtrail_rule = events.Rule(
+                self, 'CloudTrail{}Rule'.format(event_name),
+                event_pattern=events.EventPattern(
+                    source=['aws.cloudtrail'],
+                    detail_type=['AWS API Call via CloudTrail'],
+                    detail={
+                        "eventSource": [
+                            "cloudtrail.amazonaws.com"
+                        ],
+                        "eventName": [
+                            event_name
+                        ]
+                    }
+                )
+            )
+            cloudtrail_rule.add_target(targets.SnsTopic(alert_topic))
 
         self.output_props = props.copy()
 
